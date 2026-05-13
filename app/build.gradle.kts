87eff6af690368077dc2d8ef6f9d6a57a3beb59e
NaN-127 <nandurajeev3@gmail.com>
2026-05-14 02:33:35 +0530

    chore: set empty string as default for GEMINI_API_KEY in gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,7 +31,7 @@ android {
         buildConfigField(
             "String",
             "GEMINI_API_KEY",
-            "\"${localProperties.getProperty("geminiApiKey", "REMOVED_API_KEY")}\""
+            "\"${localProperties.getProperty("geminiApiKey", "")}\""
         )
         vectorDrawables.useSupportLibrary = true
     }
